c689b026d4d427cb36c12b47d743cacc3bdd5e1f
adhith <24882134+kkadhith@users.noreply.github.com>
2021-08-10 14:13:45 -0700

    initial commit.

---
+++ Matrix.h
@@ -0,0 +1,37 @@
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
+
+template <class T>
+
+class Matrix {
+    public:
+        /* Constructors:
+            (1): Default constructor
+            (2): Constructor set to m x n
+            (3): Constructor set to m x n and takes in
+                 pointer to an array of doubles.
+            (4): Copy constructor.
+            (5): Destructor.
+        */
+        Matrix();
+        Matrix(int numRows, int numColumns);
+        Matrix(int numRows, int numColumns, double *input);
+        Matrix(const Matrix &MatrixforCopy);
+        ~Matrix();
+
+        // Ability to resize matrix; returns bool
+        // T of F to determine if the resize was
+        // successful.
+        bool resize(int numRows, int numColumns);
+
+        // Returns value at m x n
+        double retrieveElement(int row, int column);
+
+        // Sets element at m x n to input value.
+        void setElement(int row, int column, double in); 
+        
+        int numRows();
+        int numColumns();
+};
+
+#endif      
