6f73da33b603099c0526ca73b601f8ecae437b83
adhith <24882134+kkadhith@users.noreply.github.com>
2021-09-09 13:06:06 -0700

    Completed MiniEigen::Vector implementation

--- src/Vector.h
+++ src/Vector.h
@@ -9,14 +9,36 @@ namespace MiniEigen {
         public:
             Vector();
             Vector(int n);
-            // Vector(int n, std::vector<double> in);
-            // Vector(const Vector &vcpy);
+            Vector(int n, std::vector<double> &in);
+            Vector(const Vector &vcpy);
     };
 
-    MiniEigen::Vector::Vector() {
-    nRows = 0;
-    nColumns = 0;
-    
+    Vector::Vector() {
+        nRows = 0;
+        nColumns = 0;    
+        nElements = 0;
+    }
+
+    Vector::Vector(int n) {
+        nRows = n;
+        nColumns = 1;
+        nElements = n;
+        std::vector<double> in(n, 0.0);
+        matrixData = in;
+    }
+
+    Vector::Vector(int n, std::vector<double> &in) {
+        nElements = n;
+        nRows = n;
+        nColumns = 1;
+        matrixData = in;
+    }
+
+    Vector::Vector(const Vector &cpy) {
+        nRows = cpy.nRows;
+        nElements = cpy.nElements;
+        nColumns = 1;
+        matrixData = cpy.matrixData;
     }
 }
 
